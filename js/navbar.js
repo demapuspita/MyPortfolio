7f10b98f41b47389872d5b91a0a104e28bd2cd05
demapuspita <demapuspita@gmail.com>
2024-03-03 09:45:36 +0700

    update

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
-    // Add 'active' class to the clicked link and remove from siblings
+    // active
     $('ul.navbar-nav a').on('click', function () {
         $(this).addClass('active').parent().siblings().find('a').removeClass('active');
+    });
+
+    // scroll
+    $(window).on('scroll', function () {
+        var scrollPos = $(document).scrollTop();
+        $('ul.navbar-nav a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr('href'));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                currLink.addClass("active").parent().siblings().find('a').removeClass('active');
+            } else {
+                currLink.removeClass("active");
+            }
+        });
     });
 });
