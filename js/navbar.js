295c29728852fcdb74484ed8a8109fe59c10b419
demapuspita <demapuspita@gmail.com>
2024-03-03 09:25:38 +0700

    update

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,6 @@
-$(document).on('click', 'ul li', function() {
-    $(this).addClass('active').siblings().removeClass('active')
-})
+$(document).ready(function () {
+    // Add 'active' class to the clicked link and remove from siblings
+    $('ul.navbar-nav a').on('click', function () {
+        $(this).addClass('active').parent().siblings().find('a').removeClass('active');
+    });
+});
